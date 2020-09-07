79d28ea6558d7fa4041fcc10497fb906889ae6bb
Alice Huh <aklee213@gmail.com>
2020-09-07 19:39:12 -0400

    [tags.js] Add tags.js in routes

---
+++ routes/api/tags.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const db = require("../../models");
+
+router.get('/', (req,res) => {
+    db.Tag.find()
+    .then(foundTag => {
+        console.log(foundTag)
+        res.send(foundTag)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status.apply(503).send({message: 'Database asleep?'})
+    })
+})
+
+
+
+module.exports = router;
