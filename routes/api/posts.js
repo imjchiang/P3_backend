2ed7ad14d2b3b5745cf409e3b9c6b226ce27256c
Alice Huh <aklee213@gmail.com>
2020-09-07 16:55:46 -0400

    [posts.js] Add put route for comemnts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,7 +9,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const db = require("../../models");
 
 router.get('/', (req,res) => {
-    db.Post.find().sort('-date')
+    db.Post.find().populate({path:'author',select:'name'}).sort('-date')
     .then(foundPost => {
         console.log(foundPost)
         res.send(foundPost)
@@ -38,10 +38,39 @@ router.post("/", (req,res) =>
     })
 })
 
+router.put('/:id/comments', (req,res)=>
+{
+    db.Post.findOneAndUpdate({
+        _id:req.params.id
+    }, 
+    {$push: 
+        {
+        comments: 
+        {
+        descriptionsAndCode: req.body.descriptionsAndCode, 
+        author: req.body.author
+        }
+        }       
+    },
+    
+    {
+        new: true
+    })
+    .then(updatedPost => {
+        res.send(updatedPost)
+        console.log(updatedPost)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(503).send({message: 'server error'})
+    })
+})
+
+
 // router.put("/id", passport.authenticate('jwt', { session: false }), (req,res) => 
 router.get('/:id', (req,res) =>
 {
-    db.Post.findById(req.params.id).populate({path:'author',select:'name'})
+    db.Post.findById(req.params.id).populate({path:'author',select:'name'}).populate({path:'tags',select:'name'})
     .then(foundPost => {
         if(foundPost){
             res.send(foundPost)
@@ -62,17 +91,21 @@ router.put('/:id', (req,res)=>
         _id:req.params.id
     },
     req.body,
+    
     {
         new: true
     })
     .then(updatedPost => {
         res.send(updatedPost)
+        console.log(updatedPost)
     })
     .catch(err => {
         console.log(err)
         res.status(503).send({message: 'server error'})
     })
 })
+
+
 
 // router.delete("/id", passport.authenticate('jwt', { session: false }), (req,res) => 
 router.delete('/:id', (req,res)=> 
